8a3b1b333c0636254c981d79165df2ace42dc78e
bucel-sebastian <bucel.ionsebastian@gmail.com>
2025-02-10 02:29:35 +0200

    [Feature] Finished product page

--- templates/woocommerce/page-product.php
+++ templates/woocommerce/page-product.php
@@ -116,9 +116,66 @@ if (post_password_required()) {
                             </svg> <span>Adaugă în coș</span>
                         </button>
                     </div>
-
-
                 </form>
+
+                <div class="product-meta">
+                    <?php
+                    $product_sku = $product->get_sku();
+                    if ($product_sku) {
+                    ?>
+                        <div class="product-meta-row">
+                            <?php
+                            echo '<span class="sku_wrapper">' . 'SKU -' . ' <span class="sku">' . $product_sku . '</span></span>';
+                            ?>
+                        </div>
+                    <?php
+                    }
+
+                    echo wc_get_product_category_list($product->get_id(), ', ', '<div class="product-meta-row">' . _n('Categorie -', 'Categorii -', count($product->get_category_ids()), 'woocommerce') . ' ', '</div>');
+
+                    echo wc_get_product_tag_list($product->get_id(), ', ', '<div class="product-meta-row">' . _n('Tag -', 'Tag-uri -', count($product->get_tag_ids()), 'woocommerce') . ' ', '</div>');
+                    ?>
+                </div>
+
+                <div class="product-shipping-notice">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                        <path d="M48 0C21.5 0 0 21.5 0 48L0 368c0 26.5 21.5 48 48 48l16 0c0 53 43 96 96 96s96-43 96-96l128 0c0 53 43 96 96 96s96-43 96-96l32 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l0-64 0-32 0-18.7c0-17-6.7-33.3-18.7-45.3L512 114.7c-12-12-28.3-18.7-45.3-18.7L416 96l0-48c0-26.5-21.5-48-48-48L48 0zM416 160l50.7 0L544 237.3l0 18.7-128 0 0-96zM112 416a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm368-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+                    </svg>
+                    <p>
+                        Livrările în Constanța sunt efectuate de către noi, iar pentru comenzile în afara orașului vă vom contacta telefonic în vederea stabilirii costului de transport.
+                    </p>
+                </div>
+
+                <div class="product-share-buttons">
+                    <h3>Distribuie acest produs</h3>
+                    <div class="products-share-buttons-container">
+                        <a class="product-share-button" style="background:#1877F2;" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_permalink()); ?>" target="_blank" class="share-button facebook">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                                <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
+                            </svg>
+                        </a>
+                        <a class="product-share-button" style="background:#000000;" href="https://twitter.com/intent/tweet?url=<?php echo urlencode(get_permalink()); ?>&text=<?php echo urlencode(get_the_title()); ?>" target="_blank" class="share-button twitter">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+                            </svg>
+                        </a>
+                        <a class="product-share-button" style="background:#0077B5;" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo urlencode(get_permalink()); ?>&title=<?php echo urlencode(get_the_title()); ?>" target="_blank" class="share-button linkedin">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                <path d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z" />
+                            </svg>
+                        </a>
+                        <a class="product-share-button" style="background:#25D366;" href="https://api.whatsapp.com/send?text=<?php echo urlencode(get_the_title() . ' ' . get_permalink()); ?>" target="_blank" class="share-button whatsapp">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
+                            </svg>
+                        </a>
+                        <a class="product-share-button" style="background:#E60023;" href="https://pinterest.com/pin/create/button/?url=<?php echo urlencode(get_permalink()); ?>&media=<?php echo urlencode(get_the_post_thumbnail_url($product->get_id(), 'full')); ?>&description=<?php echo urlencode(get_the_title()); ?>" target="_blank" class="share-button pinterest">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                                <path d="M204 6.5C101.4 6.5 0 74.9 0 185.6 0 256 39.6 296 63.6 296c9.9 0 15.6-27.6 15.6-35.4 0-9.3-23.7-29.1-23.7-67.8 0-80.4 61.2-137.4 140.4-137.4 68.1 0 118.5 38.7 118.5 109.8 0 53.1-21.3 152.7-90.3 152.7-24.9 0-46.2-18-46.2-43.8 0-37.8 26.4-74.4 26.4-113.4 0-66.2-93.9-54.2-93.9 25.8 0 16.8 2.1 35.4 9.6 50.7-13.8 59.4-42 147.9-42 209.1 0 18.9 2.7 37.5 4.5 56.4 3.4 3.8 1.7 3.4 6.9 1.5 50.4-69 48.6-82.5 71.4-172.8 12.3 23.4 44.1 36 69.3 36 106.2 0 153.9-103.5 153.9-196.8C384 71.3 298.2 6.5 204 6.5z" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
                 <?php
 
                 // woocommerce_template_single_add_to_cart();
@@ -133,6 +190,21 @@ if (post_password_required()) {
 
         </div>
 
+
+        <div class="product-how-to-order-container">
+            <div class="section-title-container">
+                <h3>
+                    Cum puteți comanda?
+                </h3>
+                <div class="section-title-breakline"></div>
+            </div>
+            <p>
+                Simplu! Fie comandați online produsele dorite, fie puteți trimite o comandă telefonică sau prin email.
+                <br />
+                Stocurile sunt limitate, așadar vă vom contacta în cazul în care nu avem pe stoc produsele dorite.
+            </p>
+        </div>
+
         <?php
         /**
          * Display product data tabs, upsells, and related products
@@ -144,9 +216,7 @@ if (post_password_required()) {
 
         <?php
 
-        $termsCategory = wc_get_product_terms($product->get_id(), 'product_cat', ['fields' => 'id']);
-
-        // echo json_encode($termsCategory);
+        $termsCategory = wc_get_product_terms($product->get_id(), 'product_cat');
 
         if (!empty($termsCategory)) {
             $args = [
@@ -157,7 +227,7 @@ if (post_password_required()) {
                     [
                         'taxonomy' => 'product_cat',
                         'field' => 'id',
-                        'terms' => $termsCategory,
+                        'terms' => $termsCategory[0]->term_id,
                     ],
                 ),
             ];
@@ -166,14 +236,15 @@ if (post_password_required()) {
 
             if ($related_category_products->have_posts()) {
         ?>
-                <div>
+                <div class="product-related-products-container">
                     <div class="section-title-container">
                         <h3>
                             Produse populare
                         </h3>
+                        <div class="section-title-breakline"></div>
                     </div>
                     <div class="products-gallery-container">
-                        <ul>
+                        <ul class="products-gallery-list">
                             <?php
                             while ($related_category_products->have_posts()) {
                                 $related_category_products->the_post();
@@ -183,10 +254,49 @@ if (post_password_required()) {
                         </ul>
                     </div>
                 </div>
-        <?php
+            <?php
             }
         }
 
+        if (!empty($termsBrand)) {
+            $args = [
+                'post_type' => 'product',
+                'posts_per_page' => 4, // Number of related products to display
+                'post__not_in' => array($product->get_id()), // Exclude the current product
+                'tax_query' => array(
+                    [
+                        'taxonomy' => 'brand',
+                        'field' => 'id',
+                        'terms' => $termsBrand[0]->term_id,
+                    ],
+                ),
+            ];
+
+            $related_brand_products = new WP_Query($args);
+
+            if ($related_brand_products->have_posts()) {
+            ?>
+                <div class="product-related-products-container">
+                    <div class="section-title-container">
+                        <h3>
+                            Te-ar mai putea interesa și
+                        </h3>
+                        <div class="section-title-breakline"></div>
+                    </div>
+                    <div class="products-gallery-container">
+                        <ul class="products-gallery-list">
+                            <?php
+                            while ($related_brand_products->have_posts()) {
+                                $related_brand_products->the_post();
+                                wc_get_template_part('content', 'product');
+                            }
+                            ?>
+                        </ul>
+                    </div>
+                </div>
+        <?php
+            }
+        }
 
         ?>
     </div>
